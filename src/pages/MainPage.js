8fd04bb8329b189a9e8478f38668c7baff7ef15e
Tomek Sośniewski <sosna86@gmail.com>
2023-03-05 19:42:21 +0100

    com4

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -17,6 +17,11 @@ const articles = [
     author:"Antoni",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab incidunt doloribus neque fugiat aut illo velit vel delectus voluptatibus cum, deserunt culpa minima? Exercitationem, rerum repellendus eaque porro consequatur quaerat!"  
 },
+    {id:4,
+    title:"Czwarty temat testowy",
+    author:"xxxx",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab incidunt doloribus neque fugiat aut illo velit vel delectus voluptatibus cum, deserunt culpa minima? Exercitationem, rerum repellendus eaque porro consequatur quaerat!"  
+},
 ]
 
 const MainPage = () => {
