1ccad5f94ab127f6fca77103761219e889fa052f
Tomek Sośniewski <sosna86@gmail.com>
2023-03-29 17:50:17 +0200

    first commit to portfolio Page

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,34 +1,11 @@
-import React, { Component } from 'react';
-import '../styles/App.css';
-import Header from './Header';
-import Navigation from './Navigation';
-import Page from './Page';
-import Footer from './Footer';
+import React from 'react'
 
-
-class App extends Component {
-  render(){ 
-  return (
-    <div className="app">
-      
-      <header>
-        {<Header/>}
-      </header>
-      <main>
-        <aside>
-          {<Navigation/>}
-        </aside>
-        <section className='page'>
-          {<Page/>}
-        </section>
-      </main>
-      <footer>
-        {<Footer/>}  
-      </footer>       
-
-   
-    </div>
-  );
-} }
+const App = () => {
+    return(
+        <>
+            <h1>dippa</h1>
+        </>
+    )
+}
 
 export default App;
