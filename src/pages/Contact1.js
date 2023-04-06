e622ecc8d42ba5fa21a646be09feeaacfa35ed05
Tomek Sośniewski <sosna86@gmail.com>
2023-04-06 19:20:50 +0200

    Form validation has been fully completed - however needs to be redesign in css

--- src/pages/Contact1.js
+++ src/pages/Contact1.js
@@ -1,10 +1,10 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import '../styles/Contact.css'
 import emailjs from '@emailjs/browser';
 
 const Contact1 = () => {
   const form = useRef();
-
+  
   const [emailSend, setEmailSend] = useState(false)
   const [inputs, setInputs] = useState({
     name:'',
@@ -18,43 +18,92 @@ const Contact1 = () => {
     formCorrect:false
   })
 
+
+  const errorMsg = {
+    nameError : "Cannot sent empty box",
+    emailError : "Email adress is wrong",
+    msgError : "Cannot sent empyt box",
+    formError : "Please correct the form prior sending"
+  }
+
   const validationCheck =() =>{ 
     let regExp = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
-      if(inputs.name !== ' '){
-        setValidation(prevState =>({...prevState, name: true }))
-          if(regExp.test(inputs.email)){
-            setValidation(prevState =>({...prevState, email: true }))
-              if(inputs.msg !==' '){
-                setValidation(prevState =>({...prevState, msg: true, formCorrect: true }))
-                
-              }
-          }
-      
-    }
-  } 
+    let name = false;
+    let email = false;
+    let msg = false;
+    let formCorrect = false; 
 
+    if(inputs.name !== ''){
+      name = true;
+    }
+    if(regExp.test(inputs.email)){
+      email = true;
+    }
+    if(inputs.msg !== ''){
+      msg = true;
+    }
+    if(name && email && msg){
+      formCorrect = true;
+    }
+    return ({
+      name,
+      email,
+      msg,
+      formCorrect
+    })
+    
+    // if(inputs.name !== ''){       
+    //     setValidation(prevState =>({...prevState, name: true }))
+    //     console.log(validation.name)
+    //       if(regExp.test(inputs.email)){
+    //         console.log(validation.email)
+    //         setValidation(prevState =>({...prevState, email: true }))
+    //           if(inputs.msg !==''){
+    //             console.log(validation.msg)
+    //             setValidation(prevState =>({...prevState, msg: true, formCorrect: true }))                
+    //           }
+    //       }      
+    // }
+  } 
+  // useEffect (() => validationCheck(),[inputs])
+  
   const inputHandler = (e) => {
-    setInputs({...inputs, [e.target.name]: e.target.value} )
-    validationCheck()
+    setInputs({...inputs, [e.target.name]: e.target.value} )    
   }
 
 
   const sendEmail = (e) => {
     e.preventDefault();
-    
-    console.log(validation.formCorrect)
-    if(validation.formCorrect){
-    emailjs.sendForm('service_vjms4p7', 'template_lunj1dtODTADMNIESKASUJ', form.current, 'xXPrA9aIKQNlNKQtF')
+    const formValidation = validationCheck()
+    console.log(formValidation)
+    if(formValidation.formCorrect){
+    emailjs.sendForm('service_vjms4p7', 'template_lunj1dt', form.current, 'xXPrA9aIKQNlNKQtF')
       .then((result) => {
-
           setEmailSend(!emailSend)
-
+          setInputs({ 
+          name:'',
+          email:'',
+          msg:''})
+          setValidation({
+            name:false,
+            email:false,
+            msg:false,
+            formCorrect:false
+          })
       }, (error) => {
           console.log(error.text);
       });
       e.currentTarget.reset()
-      setEmailSend(!emailSend)
-  }};
+      // setEmailSend(!emailSend)
+  }else {
+    setValidation({
+      name:!formValidation.name,
+      email:!formValidation.email,
+      msg:!formValidation.msg,
+      formCorrect:!formValidation.formCorrect
+    })
+  }
+};
 
   return (
     
@@ -67,15 +116,15 @@ const Contact1 = () => {
     <form ref={form} onSubmit={sendEmail}>
         <div>
         <label htmlFor='nameInput'>Name: </label>
-        <input type="text" name='name' id='nameInput' onChange={inputHandler} value={inputs.name} />
+        <input type="text" name='name' id='nameInput' onChange={inputHandler} value={inputs.name} /> {validation.name && <span>{errorMsg.nameError}</span>}
         </div>
         <div>
         <label htmlFor='emailInput'>Email: </label>
-        <input type="email" name="email" id='emailInput' onChange={inputHandler} value={inputs.email} />
+        <input type="email" name="email" id='emailInput' onChange={inputHandler} value={inputs.email} />{validation.email && <span>{errorMsg.emailError}</span>}
         </div>
         <div>
         <label htmlFor='msgInput'>Message: </label>
-        <textarea name="msg" id='msgInput' rows="4" cols="33" onChange={inputHandler} value={inputs.msg} />
+        <textarea name="msg" id='msgInput' rows="4" cols="33" onChange={inputHandler} value={inputs.msg} />{validation.msg && <span>{errorMsg.msgError}</span>}
         </div>
         <input type="submit" value="Send" id='submitBtn'  />
     </form>
